fb28b51e35ed49e217548527cc5397f249d11509
dragGil <59195578+dragGil@users.noreply.github.com>
2020-07-06 23:08:45 +0900

    1강
    완료

---
+++ CUBIC MUSIC/Assets/Script/CenterFrame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFrame : MonoBehaviour
+{
+    AudioSource myAudio;
+    bool musicStart = false;
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+                        Debug.Log("collision enter");
+
+        if(musicStart == false)
+        { 
+            if(collision.CompareTag("Note"))
+            {
+                Debug.Log("music start");
+                myAudio.Play();
+                musicStart = true;
+            }
+        }
+        
+    }
+}
---
+++ CUBIC MUSIC/Assets/Script/Manager/NoteManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d; // 사소한 오차에도 문제가 생기기때문에 플롯보다 오차가 더 작은 double 사용
+    [SerializeField] Transform tfNoteappear = null;
+    [SerializeField] GameObject goNote = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 60 / 120 = 1beat per 0.5 seconds
+        // 60s / bpm = 1beat 시간
+        currentTime += Time.deltaTime;
+        if(currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteappear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d/bpm; // 0으로 초기화하면 안됨 시간차 손실
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        if(collision.CompareTag("Note"))
+            Destroy(collision.gameObject);
+    }
+}
